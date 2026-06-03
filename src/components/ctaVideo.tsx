f604aa46b59d703c591064448e769e8f89c6968c
tegrakmd <tegrakamanda@gmail.com>
2026-06-03 13:56:46 +0200

    ajusteage 4 Cta H

--- src/components/ctaVideo.tsx
+++ src/components/ctaVideo.tsx
@@ -4,7 +4,7 @@ import { CTA_VIDEO } from "@/datas/videos"
 export function Cta() {
   return (
     <section id="video" className="px-5  pb-0 sm:px-6 lg:px-10">
-      <div className="relative aspect-[2.3/1] min-h-[700] w-full overflow-hidden rounded-[14px]">
+      <div className="relative aspect-[2.3/1] min-h-[600] w-full overflow-hidden rounded-[14px]">
         <div className="absolute inset-0">
           <LazyVideo
             src={CTA_VIDEO}
